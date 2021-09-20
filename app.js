d7b8fa5c9d2aa762a2ac30529af8fe63d3f35c15
Adarsh2k21 <adarsh.patel9619@gmail.com>
2021-09-20 20:46:01 +0530

    added footer

--- app.js
+++ app.js
@@ -1,63 +1,75 @@
+// const { object } = require("prop-types");
+
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
+const noticeMsg = document.querySelector(".notice");
+const closeButton = document.querySelector("#clos");
+
 const lucky = document.querySelector(".lucky-img");
 const unLucky = document.querySelector(".unlucky-img");
 
-function compareValues(sum, luckyNumber){
-    
-    if(sum%luckyNumber===0){
+closeButton.addEventListener("click", close);
+
+function close() {
+    console.log("click");
+    noticeMsg.style.display = "none";
+}
+
+
+
+function compareValues(sum, luckyNumber) {
+    // hide();
+
+    if (sum % luckyNumber === 0) {
         outputBox.innerText = "You are a lucky person!!!🤩";
 
-        // document.getElementsByClassName(".lucky-img").style.display = "block";
-        // document.getElementsByClassName(".unlucky-img").style.display = "none";
-        
-         lucky.style.display = "block";
-         unLucky.style.display = "none";
+        var img = document.createElement('img');
+        img.src = "happy-dog.jpg";
+        document.querySelector('#output-box').appendChild(img);
 
-    }else{
-        outputBox.innerText = "Oops you are unlucky!!!🙃";
+    } else {
+        outputBox.innerText = "Oops you are not lucky 🙃 but you will make your own luck😉!!!";
 
-        // document.getElementsByClassName(".unlucky-img").style.display = "block";
-        // document.getElementsByClassName(".lucky-img").style.display = "none";
-        
-        unLucky.style.display = "block";
-        lucky.style.display = "none";
-}
+        var img = document.createElement('img');
+        img.src = "sad-dog.jpg";
+        document.querySelector('#output-box').appendChild(img);
+
+    }
 }
 
 
 
 checkNumberButton.addEventListener("click", check)
-    // console.log(dateOfBirth.value, luckyNumber.value);
-    
+// console.log(dateOfBirth.value, luckyNumber.value);
 
 
-function calculateSum(dob){
-        dob = dob.replaceAll("-", "");
-        let  sum = 0;
-        for (let index = 0; index < dob.length; index++) {
-            sum = sum + Number(dob.charAt(index));
-            
-        }
-        return sum;
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+
     }
+    return sum;
+}
 
-function check(){
+function check() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    if (sum&&dob) {
+    if (sum && dob) {
         compareValues(sum, luckyNumber.value)
-    }else{
+    } else {
         outputBox.innerText = "please enter both the fields";
     }
-    
+
 }
 
-function hide() {
-    // outputBox.style.display = "none";
-    unLucky.style.display = "none";
-        lucky.style.display = "none";
-}
+// function hide() {
+//     // outputBox.style.display = "none";
+//     unLucky.style.display = "none";
+//         lucky.style.display = "none";
+// }
